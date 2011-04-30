53b531c37a15b76d92c54cb09ca1871ff68619d3
David Heitzman <daveheitzman@yahoo.com>
2011-04-30 08:15:42 -0500

    initial creation of repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AptifugeCom::Application.config.secret_token = '7e83fba28065d29bee8e6d5a32fd53335be3e2dfadeab18abcbc9c506a4929f8dd426a77150047ba26eb64e5724c80ed31e57dd2c3f26297589466d662cc9b83'
